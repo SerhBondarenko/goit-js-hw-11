a0a7c10666eea36d00152d0821921e11314a087d
SerhBondarenko <serhii.bondarenk@gmail.com>
2021-12-10 11:49:15 +0200

    5

--- src/index.js
+++ src/index.js
@@ -1,14 +1,86 @@
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
-import API from './fetchImage';
-const axios = require('axios');
-import Notiflix from 'notiflix';
+//import API from './fetchImage';
+//const axios = require('axios');
+//import Notiflix from 'notiflix';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import imagesTpl from './template.hbs';
 import ApiServise from './fetchImage';
-import LoadMoreButton from './load-more-btn'
+//import LoadMoreButton from './load-more-btn'
 
-const form = document.querySelector('#search-form')
+const per_page=40;
+let displayedHits=0;
+const form = document.querySelector('.search-form')
+const buttonMore = document.querySelector('.load-more');
+const cardList = document.querySelector('.gallery')
+const apiServise = new ApiServise()
+
+form.addEventListener('submit', onSearch);
+buttonMore.addEventListener('click', onButtonMore);
+
+function onSearch (e)  {
+   e.preventDefault();
+  clearContainer()
+   apiServise.query = e.currentTarget.elements.searchQuery.value.trim();
+   apiServise.resetPage();
+   apiServise.fetchImage().then(renderUserList)
+   clearContainer()
+   
+}
+
+  function onButtonMore () {
+      apiServise.fetchImage().then(renderUserList).then(pageScroll)
+  }
+  
+  function renderUserList(data) {
+    buttonMore.classList.add('is-hidden');
+    cardList.insertAdjacentHTML('beforeend', imagesTpl(data.hits))
+      displayedHits += data.hits.length;
+    
+    lightboxGallery()
+    if (apiServise.query ==="" || data.totalHits ===0){
+      buttonMore.classList.remove('is-hidden')
+      clearContainer()
+      return error()
+     }
+  
+    if (data.totalHits < per_page || displayedHits === data.totalHits) {
+       Notify.warning('We areorry, but you have reached  sthe end of search results.');
+      buttonMore.classList.remove('is-hidden');
+    }
+    else {
+      Notify.success(`Hooray! We found ${data.totalHits} images.`)
+    }
+ }
+//очищает контейне если нечего не найдено 
+ function clearContainer() {
+  cardList.innerHTML ="";
+}
+///Если бэкенд возвращает пустой массив, 
+ function error() {
+  return Notify.failure('Sorry, there are no images matching your search query. Please try again.')
+ }
+ ///Прокрутка страницы
+function pageScroll() {
+  const { height: cardHeight } = cardList.firstElementChild.getBoundingClientRect();
+  window.scrollBy({
+    top: cardHeight * 2,
+    behavior: 'smooth',
+  });
+  
+}
+///Библиотека SimpleLightbox
+function lightboxGallery(){
+  var lightbox = new SimpleLightbox('.gallery a', { 
+    captions: true,
+    captionsData:"alt",
+    captionPosition:'bottom',
+    captionDelay: 250,/* options */ });
+}
+
+
+
+/* const form = document.querySelector('#search-form')
 //const searchBtn = document.querySelector("button");//
 const input = document.querySelector("input");
 const gallery = document.querySelector('.gallery')
@@ -39,7 +111,7 @@ function onSubmit(e) {
 loadMoreBtn.show();
   loadMoreBtn.disable();
   apiServise.resetPage();
-  apiServise.fetchImage().then(renderItems).then(loadMoreBtn.enable()).catch((error) => { return Notify.failure("Sorry, there are no images matching your search query. Please try again.")});
+  apiServise.fetchImage().then(renderItems).then(loadMoreBtn.enable());
     };
 
 function onLoadMore(e) {
@@ -48,13 +120,22 @@ function onLoadMore(e) {
    if (apiServise.query === '') {
     return alert('enter something')
   };
-  apiServise.fetchImage().then(renderItems).then(loadMoreBtn.enable()).catch((error) => { return Notify.failure("Sorry, there are no images matching your search query. Please try again.")});
+  apiServise.fetchImage().then(renderItems).then(loadMoreBtn.enable());
 };
 
 function renderItems(data) {
   gallery.insertAdjacentHTML('beforeend', imagesTpl(data.hits));
   Notify.success(`Hooray! We found ${data.totalHits} images.`)
-    lightboxGallery()
+  lightboxGallery();
+  
+ if (apiServise.query ==="" || data.totalHits ===0){
+   clearContainer();
+   return error();
+     }
+     displayedHits += data.hits.length;
+    if (data.totalHits < per_page || displayedHits === data.totalHits) {
+       Notify.warning('We are sorry, but you have reached the end of search results.');
+     }
 };
 
 function clearImgContainer() {
@@ -80,5 +161,5 @@ function lightboxGallery(){
     captions: true,
     captionsData:"alt",
     captionPosition:'bottom',
-    captionDelay: 250,/* options */ });
-}
+    captionDelay: 250});}*/
+ 
